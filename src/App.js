1d4dc22f5837adc3c4dfa41eda84f06e5ff293e4
Joe <jo841130s@gmail.com>
2022-11-06 14:25:22 +0800

    Add routing setting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Home from "./pages/Home";
+// import AddVendor from "./pages/AddVendor";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          {/* <Route exact path="/addVendor" element={<AddVendor />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
