7cbe926303e121b2e91ea2f40f85c508d469ee87
Joe <jo841130s@gmail.com>
2023-03-29 23:08:14 +0800

    Fix files name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Home from "./pages/Home";
-import JoinUs from "./pages/JoinUs";
+// import JoinUs from "./pages/JoinUs";
 // import AddVendor from "./pages/AddVendor";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Explore from "./pages/Explore";
+import JoinUs from "./pages/JoinUs";
 import ScrollToTop from "./components/ScrollToTop";
 import Experience from "./pages/Experience";
 import Menu from "./pages/Menu";
@@ -14,8 +14,8 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/explore" element={<Explore />} />
-          <Route exact path="/joinus" element={<Explore />} />
+          {/* <Route exact path="/explore" element={<Explore />} /> */}
+          <Route exact path="/joinus" element={<JoinUs />} />
           <Route exact path="/experience" element={<Experience />} />
           <Route exact path="/experiences/:experienceId" element={<Menu />} />
           {/* <Route exact path="/addVendor" element={<AddVendor />} /> */}
--- src/pages/JoinUs/index.js
+++ src/pages/JoinUs/index.js
@@ -1,145 +1,253 @@
+import React, { useState } from "react";
 import styles from "./index.module.scss";
-import { Button } from "@mui/material";
-import banner from "../../images/banner0.jpg";
-import lineImg from "../../images/line.png";
-import phoneImg from "../../images/phone.png";
-import mailImg from "../../images/email.png";
+import banner from "../../images/banner.jpg";
+import banner2 from "../../images/banner2.jpg";
+import creator1 from "../../images/creators/1.jpg";
+import creator2 from "../../images/creators/2.jpg";
+import creator3 from "../../images/creators/3.jpg";
+import eric from "../../images/vendors/eric.png";
+import joe from "../../images/vendors/joe.png";
+import gary from "../../images/vendors/gary.png";
 import Footer from "../../components/Footer";
+import { useNavigate } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import { child, get, getDatabase, ref, set } from "firebase/database";
 
 const JoinUs = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const creator = [
+    {
+      title: "熱愛料理",
+      subtitle:
+        "你樂於烹飪，會挑戰各種菜系、嘗試不同方式、運用在地食材創造屬於你的料理。你可以把這些喜悅分享給更多人，透過料理進行文化交流是個不可思議的過程。",
+      image: creator1,
+    },
+    {
+      title: "有獨特性",
+      subtitle:
+        "你擁有獨一無二的生活風格，或許你是一個全職 DJ，或許你是一個古董收藏家，或許你是個業餘咖啡師。把這樣的獨特生活結合料理，帶給你的客人一段超越味蕾的體驗。",
+      image: creator2,
+    },
+    {
+      title: "樂於分享",
+      subtitle:
+        "你是個樂於分享且好客的人，會邀請親朋好友來到你的空間用餐、舉辦聚餐活動，與大家分享你的料理以及故事，是一段經由美食的交流，共同創造難忘的美好回憶。",
+      image: creator3,
+    },
+  ];
+  const benefits = [
+    {
+      number: "01",
+      title: "興趣化為收入",
+      subtitle:
+        "可以把你對料理的興趣轉換為新的收入來源，不需要擁有一間餐廳，不需要聘請員工，不需要行銷預算，只需要對料理的熱情，透過 Gatherd 簡單的上架流程成為一個美食創作家。",
+    },
+    {
+      number: "02",
+      title: "一切自己做主",
+      subtitle:
+        "調整自如的時程安排，不受限制的菜單設計，建立自己的料理模式，賦予美食創作家接受客人預訂與否的權利，我們希望給你有最大的自由，讓你專心在創意上。",
+    },
+    {
+      number: "03",
+      title: "用食物說故事",
+      subtitle:
+        "你可以在 Gatherd 上讓客人知道你是個怎麼樣的人，透過菜單的設計、食物的照片、自我介紹的文字，都會是我們呈現你的獨特的最佳方式，讓客人在體驗之前就已經對你充滿想像。",
+    },
+    {
+      number: "04",
+      title: "分享給更多人",
+      subtitle:
+        "透過 Gatherd 創造更多分享廚藝的機會，盡情展現你的料理，我們會幫助你找到一樣熱愛美食，並且欣賞你創造能力的人，不斷有新的發現、新的認識，這對你來說會是一場探索之旅。",
+    },
+    {
+      number: "05",
+      title: "共學共好",
+      subtitle:
+        "加入懷有相同喜好人組成的群體，彼此分享想法和靈感。大家都對食物抱有熱情，相互交流、協作、成長和學習，這樣的過程會讓你更具有創造力，激發意想不到的火花。",
+    },
+    {
+      number: "06",
+      title: "建立自己的社群",
+      subtitle:
+        "讓喜愛你料理的人感覺到你對烹飪的熱忱，與他們一起探索不一樣的聚餐體驗，讓他們從中被啟發並感到有趣。聚餐之前或許互不相識，但經歷這段體驗後，你們會成為有共同回憶的好友。",
+    },
+  ];
+  const how = [
+    {
+      number: "1",
+      title: "設計菜單",
+      subtitle:
+        "發揮你的想像力，設計一個專屬於你的菜單。不限於任何種類的料理或用餐形式，將你的熱情和創意在這裡展現。",
+    },
+    {
+      number: "2",
+      title: "選擇用餐空間",
+      subtitle:
+        "找到一個適合你料理的空間。可以是自己的住家、親朋好友的場地或是任何其他可以激發你的創造力的地方。與跟你一樣熱愛美食的人們在這裏一起享受料理。",
+    },
+    {
+      number: "3",
+      title: "開始聚餐體驗",
+      subtitle:
+        "只要你準備好了，通過簡單又快速的上架流程，就可以加入我們成為美食創作家，讓我們一起期待未來的旅程！",
+    },
+  ];
+  const vendors = [
+    { name: "Eric C", image: eric },
+    { name: "Joe H", image: joe },
+    { name: "Gary L", image: gary },
+  ];
+  const firebaseConfig = {
+    databaseURL:
+      "https://gatherd-test-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
+  const dbRef = ref(db);
+
+  const handleSaveEmail = () => {
+    get(child(dbRef, "/"))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const emails = snapshot.val().emails || [];
+          emails.push(email);
+          set(ref(db, "emails"), emails);
+          setEmail("");
+          alert("成功！");
+        } else {
+          alert("失敗請稍後再試！");
+        }
+      })
+      .catch((error) => {
+        alert("失敗請稍後再試！");
+      });
+  };
+
   return (
-    <div className={styles.container}>
+    <>
       <div className={styles.banner}>
+        <img className={styles.backgroundImg} src={banner} alt="" />
+        <div className={styles.cover} />
         <div className={styles.text}>
-          <div className={styles.title}>分享你的美食創作</div>
+          <div className={styles.title}>創造不一樣的聚餐體驗</div>
           <div className={styles.subtitle}>
-            熱愛美食的創作者們，敞開你們的家門一同與你的親朋友好友、鄰居和其他美食熱愛者分享你獨特的傳統和創意佳餚！
+            讓每個熱愛烹飪的人，可以在自己的空間，與來自各地、喜歡美食的人們分享你的料理和故事。
           </div>
-        </div>
-        <div className={styles.image}>
-          <img src={banner} alt="" />
+          <button
+            className={styles.button}
+            onClick={() => window.open("https://lin.ee/CGfqTpp")}
+          >
+            加入我們
+          </button>
         </div>
       </div>
-      <div className={styles.info}>
-        <div className={styles.infoTitle}>如何成為 Gatherd 美食創作家</div>
-        <div className={styles.steps}>
-          <div className={styles.lineContainer}>
-            <div className={styles.line} />
-          </div>
-          <div className={styles.step}>
-            <div className={styles.text}>
-              <div className={styles.number}>1</div>
-              <div className={styles.title}>食安字號申請</div>
-              <div className={styles.content}>
-                為了保護美食創作家的權利，食安字號為販售、運輸食品相關行為時，政府規定必須擁有之憑證。點擊以下連接申請。
-              </div>
-            </div>
-            <Button
-              variant="contained"
-              onClick={() => window.open("https://forms.gle/9dDT8j4Baospn21n6")}
-            >
-              馬上申請
-            </Button>
-          </div>
-          <div className={styles.step}>
-            <div className={styles.text}>
-              <div className={styles.number}>2</div>
-              <div className={styles.title}>美食創作家帳號申請</div>
-              <div className={styles.content}>
-                讓我們以及熱愛美食者們可以更了解你的美食創作歷程，請提供美食創作家上路相關資訊。由於我們很重視照片的品質，因此照片會在另一個表單上傳，請注意！
-              </div>
-            </div>
-            <Button
-              variant="contained"
-              onClick={() => window.open("https://forms.gle/7VWm5qSBZMBQAP8m8")}
-            >
-              開始探索
-            </Button>
-          </div>
-          <div className={styles.step}>
-            <div className={styles.text}>
-              <div className={styles.number}>3</div>
-              <div className={styles.title}>Meet & Dine 套餐申請</div>
-              <div className={styles.content}>
-                把你最拿手的傳統和創億佳餚分享給不同讓熱愛美食者們。請提供你的套餐資訊。由於我們很重視照片的品質，因此照片會在另一個表單上傳，請注意！
-              </div>
-            </div>
-            <Button
-              variant="contained"
-              onClick={() => window.open("https://forms.gle/1xz3zwDKQvbcpTgF9")}
-            >
-              馬上申請
-            </Button>
-          </div>
-          <div className={styles.step}>
-            <div className={styles.text}>
-              <div className={styles.number}>4</div>
-              <div className={styles.title}>照片上傳</div>
-              <div className={styles.content}>
-                為了讓你的照片在網頁上有更好的呈現，請遵守我們規範，將你準備好的照片上傳到這裡，才算是真正成為美食創作家/完成菜單上架！
-              </div>
-            </div>
-            <Button
-              variant="contained"
-              onClick={() => window.open("https://forms.gle/fLCWENCt2q54Jo5s8")}
-            >
-              前往上傳
-            </Button>
+      <div className={styles.why}>
+        <div className={styles.title}>
+          <div>「飲食造就社會。」</div>
+          <div>
+            <div className={styles.dash}>——</div>安東尼波登
           </div>
         </div>
-        <div className={styles.steps}>
-          <div className={styles.lineContainer}>
-            <div className={styles.line2} />
-          </div>
-          <div className={styles.step}>
-            <div className={styles.text}>
-              <div className={styles.number}>5</div>
-              <div className={styles.title}>編輯個人資料</div>
+        <div className={styles.subtitle}>
+          每個人都有獨特的料理方式，賦予食物獨一無二的樣貌，讓這些料理被分享、看到及品嚐，讓這些味道成為一段段的記憶，也成為人與人之間交流的橋樑。
+        </div>
+      </div>
+      <div className={styles.creator}>
+        <div className={styles.creatorTitle}>你是美食創作家嗎？</div>
+        <div className={styles.sections}>
+          {creator.map((c) => (
+            <div className={styles.section} key={c.title}>
+              <img src={c.image} alt="" />
               <div className={styles.content}>
-                如有想更新你的美食創作歷程或其他聯繫資訊，請點擊此連結來編輯個人內容。
+                <div className={styles.title}>{c.title}</div>
+                <div className={styles.subtitle}>{c.subtitle}</div>
               </div>
             </div>
-            <Button
-              variant="contained"
-              onClick={() => window.open("https://forms.gle/YocJTC12YPzndUe88")}
-            >
-              前往編輯
-            </Button>
-          </div>
-          <div className={styles.step}>
-            <div className={styles.text}>
-              <div className={styles.number}>6</div>
-              <div className={styles.title}>編輯 Meet & Dine 套餐</div>
-              <div className={styles.content}>
-                如有想提供或更新你的傳統和創億佳餚，請點擊此連結來編輯套餐內容。
-              </div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.benefits}>
+        <div className={styles.title}>加入Gatherd能夠...</div>
+        <div className={styles.container}>
+          {benefits.map((benefit) => (
+            <div className={styles.section} key={benefit.number}>
+              <div className={styles.number}>{benefit.number}</div>
+              <div className={styles.title}>{benefit.title}</div>
+              <div className={styles.subtitle}>{benefit.subtitle}</div>
             </div>
-            <Button
-              variant="contained"
-              onClick={() => window.open("https://forms.gle/q5ah7SgC6cyoL9226")}
-            >
-              前往編輯
-            </Button>
+          ))}
+        </div>
+      </div>
+      <div className={styles.problem}>
+        <div className={styles.text}>
+          <div className={styles.subtitle}>
+            致力於為美食愛好者創造機會，與志同道合的人們建立新的關係與交流。
           </div>
-          <div className={styles.step}>
-            <div className={styles.text}>
-              <div className={styles.number}>7</div>
-              <div className={styles.title}>LINE 官方帳號</div>
-              <div className={styles.content}>
-                為了提供美食創作家最即時的服務，提供了LINE的官方帳號，讓你的創作流程可以更加順利。點擊以下連接了解加入官方帳號。
-              </div>
+          <button onClick={() => window.open("https://lin.ee/CGfqTpp")}>
+            加入我們
+          </button>
+        </div>
+        <div className={styles.image}>
+          <img src={banner2} alt="" />
+        </div>
+      </div>
+      {/* <div className={styles.vendors}>
+        <div className={styles.title}>立即體驗</div>
+        <div className={styles.subtitle}>
+          馬上來認識我們的美食創作家，每一位都有自己的特色，在加入我們之前可以先來試這會是個怎麼樣的體驗。
+        </div>
+        <div className={styles.pictures}>
+          {vendors.map((vendor) => (
+            <div
+              className={styles.vendor}
+              key={vendor.name}
+              onClick={() => navigate("../")}
+            >
+              <img src={vendor.image} alt="" />
+              <div className={styles.name}>{vendor.name}</div>
             </div>
-            <Button
-              variant="contained"
-              onClick={() => window.open("https://lin.ee/CGfqTpp")}
-            >
-              立即加入
-            </Button>
+          ))}
+        </div>
+      </div> */}
+      <div className={styles.how}>
+        <div className={styles.title}>如何加入Gatherd?</div>
+        <div className={styles.sections}>
+          {how.map((h) => (
+            <div className={styles.section} key={h.number}>
+              <div className={styles.number}>{h.number}</div>
+              <div className={styles.sectionTitle}>{h.title}</div>
+              <div className={styles.sectionSubtitle}>{h.subtitle}</div>
+            </div>
+          ))}
+        </div>
+        <button
+          className={styles.button}
+          onClick={() => window.open("https://lin.ee/CGfqTpp")}
+        >
+          加入我們
+        </button>
+      </div>
+      <div className={styles.sendEmail}>
+        <div className={styles.text}>
+          <div className={styles.title}>了解更多</div>
+          <div className={styles.subtitle}>
+            請留下電子信箱，我們將分享更多相關資訊，或邀請你與其他美食創作家一同參加我們舉辦的體驗及活動，並且解答你所有的疑問。
           </div>
+        </div>
+        <div className={styles.input}>
+          <input
+            placeholder="電子信箱"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button onClick={handleSaveEmail}>送出</button>
         </div>
       </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
