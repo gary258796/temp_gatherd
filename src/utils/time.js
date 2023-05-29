7c2a461e0254e9a1e72fe405a34fa93b60f9b96e
Joe <jo841130s@gmail.com>
2023-05-29 22:48:47 +0800

    Modify display dates

--- src/utils/time.js
+++ src/utils/time.js
@@ -2,5 +2,6 @@ import moment from "moment";
 
 export const handleDisplayTimes = (times) => {
   const today = new Date();
+  today.setDate(today.getDate() + 3);
   return times.filter((time) => moment(time.id).toDate() > today);
 };
