79123706dba97fc3170167d8174616f19e31f332
Joe <jo841130s@gmail.com>
2023-06-20 15:00:00 +0800

    Save user data to firebase

--- src/components/LoginModal/index.js
+++ src/components/LoginModal/index.js
@@ -2,14 +2,25 @@ import { saveUserData } from "../../utils/user";
 import styles from "./index.module.scss";
 import { GoogleLogin } from "@react-oauth/google";
 import jwt_decode from "jwt-decode";
+import { app } from "../../constants/FirebaseStorage";
+import { getDatabase, ref, set } from "firebase/database";
 
 const LoginModal = (props) => {
   const { onUserChange, onClose } = props;
+  const db = getDatabase(app);
 
   const handleLogin = async (response) => {
     saveUserData(response.credential);
     const user = jwt_decode(response.credential);
-    onUserChange(user);
+    const { email, name, picture } = user;
+    const path = `users/${user.email.split("@")[0]}`;
+    const params = {
+      email,
+      name,
+      picture,
+    };
+    await set(ref(db, path), params);
+    onUserChange(params);
   };
 
   return (
