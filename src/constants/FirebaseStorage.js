78ea923f7ac466dbbcee2a2967acfe9a15926411
Joe <jo841130s@gmail.com>
2023-09-30 19:20:38 +0800

    Reset project

--- src/constants/FirebaseStorage.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  databaseURL:
-    "https://gatherd-test-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  storageBucket: "gs://gatherd-test.appspot.com",
-};
-
-export const app = initializeApp(firebaseConfig);
--- src/utils/time.js
+++
@@ -1,7 +0,0 @@
-import moment from "moment";
-
-export const handleDisplayTimes = (times) => {
-  const today = new Date();
-  today.setDate(today.getDate() + 2);
-  return times.filter((time) => moment(time.id).toDate() > today);
-};
